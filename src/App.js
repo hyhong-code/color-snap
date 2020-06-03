1e390128241b33c9793921476636eb85ca09eeb1
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-03 13:32:38 -0700

    add Palette component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Palette from "./Palette";
+import seedPalettes from "./seedPalettes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Palette {...seedPalettes[4]} />
+    </div>
+  );
 }
 
 export default App;
