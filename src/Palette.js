c77a466a28061b0bb32dcb0e5a00e021c83eb524
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-03 16:16:09 -0700

    add styles to RC Slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import "./Palette.css";
+
 // RC Slider
-import Slider, { Range } from "rc-slider";
+import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import "./Palette.css";
 
 class Palette extends Component {
   constructor(props) {
@@ -30,13 +31,15 @@ class Palette extends Component {
 
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
         {/* Navbar */}
         <div className="Palette-colors">{colorBoxes()}</div>
         {/* Footer */}
