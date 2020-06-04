6332d967abf44b939e96b2f9af5f07a54a0987c4
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-04 01:57:29 -0700

    separate withStyle into own js files

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,136 +1,8 @@
 import React, { Component } from "react";
-import Chroma from "chroma-js";
 import { Link } from "react-router-dom";
+import styles from "./styles/ColorBoxStyles";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { withStyles } from "@material-ui/core/styles";
-import "./ColorBox.css";
-
-const styles = {
-  colorBox: {
-    width: "20%",
-    height: (props) => (props.showingFullPalette ? "25%" : "50%"),
-    margin: "0 auto",
-    display: "inline-block",
-    cursor: "pointer",
-    position: "relative",
-    marginBottom: "-3.5px",
-    "&:hover button": {
-      opacity: 1,
-    },
-  },
-  copyText: {
-    color: (props) =>
-      Chroma(props.background).luminance() >= 0.7
-        ? "rgba(0, 0, 0, 0.5)"
-        : "white",
-  },
-  colorName: {
-    color: (props) =>
-      Chroma(props.background).luminance() <= 0.08
-        ? "white"
-        : "rgba(0, 0, 0, 0.75)",
-  },
-  showMore: {
-    color: (props) =>
-      Chroma(props.background).luminance() >= 0.7
-        ? "rgba(0, 0, 0, 0.5)"
-        : "white",
-    background: "rgba(255, 255, 255, 0.3)",
-    position: "absolute",
-    border: "none",
-    right: "0",
-    bottom: "0",
-    width: "60px",
-    height: "30px",
-    textAlign: "center",
-    lineHeight: "30px",
-    textTransform: "uppercase",
-  },
-  copyButton: {
-    color: (props) =>
-      Chroma(props.background).luminance() >= 0.7
-        ? "rgba(0, 0, 0, 0.5)"
-        : "white",
-    width: "100px",
-    height: "30px",
-    position: "absolute",
-    display: "inline-block",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    textAlign: "center",
-    outline: "none",
-    border: "none",
-    background: "rgba(255, 255, 255, 0.3)",
-    fontSize: "1rem",
-    lineHeight: "30px",
-    textTransform: "uppercase",
-    cursor: "pointer",
-    textDecoration: "none",
-    opacity: 0,
-  },
-  boxContent: {
-    position: "absolute",
-    width: "100%",
-    left: "0",
-    bottom: "0",
-    padding: "10px",
-    color: "black",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "0.75rem",
-  },
-  copyOverlay: {
-    opacity: "0",
-    zIndex: "0",
-    width: "100%",
-    height: "100%",
-    transform: "scale(0.1)",
-    transition: "transform 0.5s ease-in-out",
-  },
-  showOverlay: {
-    opacity: "1",
-    transform: "scale(50)",
-    zIndex: "10",
-    position: "absolute",
-  },
-  copyMsg: {
-    position: "fixed",
-    left: "0",
-    right: "0",
-    top: "0",
-    bottom: "0",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    fontSize: "4rem",
-    transform: "scale(0.1)",
-    opacity: "0",
-    color: "white",
-  },
-  showMsg: {
-    opacity: "1",
-    transform: "scale(1)",
-    zIndex: "15",
-    transition: "all 0.4s ease-in-out",
-    transitionDelay: "0.3s",
-    "& h1": {
-      fontWeight: "400",
-      textShadow: "1px 2px black",
-      backgroundColor: "rgba(255, 255, 255, 0.2)",
-      width: "100%",
-      textAlign: "center",
-      marginBottom: "0",
-      padding: "1rem",
-      textTransform: "uppercase",
-    },
-    "& p": {
-      fontSize: "2rem",
-      fontWeight: "100",
-    },
-  },
-};
 
 class ColorBox extends Component {
   constructor(props) {
@@ -158,8 +30,6 @@ class ColorBox extends Component {
       classes,
     } = this.props;
     const { showOverlay } = this.state;
-    const isDarkColor = Chroma(background).luminance() <= 0.08;
-    const isLightColor = Chroma(background).luminance() >= 0.7;
 
     return (
       <CopyToClipboard text={background} onCopy={this.handleCopy}>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,48 +3,8 @@ import { Link } from "react-router-dom";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
+import styles from "./styles/PaletteStyles";
 import { withStyles } from "@material-ui/core/styles";
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
-  colors: {
-    height: "90%",
-  },
-  back: {
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    cursor: "pointer",
-    position: "relative",
-    marginBottom: "-3.5px",
-    opacity: 1,
-    backgroundColor: "black",
-    "& a": {
-      color: "white",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      textAlign: "center",
-      outline: "none",
-      border: "none",
-      background: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      textTransform: "uppercase",
-      cursor: "pointer",
-      textDecoration: "none",
-    },
-  },
-};
 
 class SingleColorPalette extends Component {
   constructor(props) {
